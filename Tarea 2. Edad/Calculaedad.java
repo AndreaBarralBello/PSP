188482f5a028a3e043e96c5f97af691356907020
Andrea Barral <andrea.barral.bello@gmail.com>
2026-09-22 01:30:57 +0200

    Calcular edad

--- Tarea 2. Edad/Calculaedad.java
+++ Tarea 2. Edad/Calculaedad.java
@@ -1,31 +1,68 @@
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Calculaedad {
-    public static void main(String[] args) throws IOException{
 
-		Process process = null;
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
 
-	try {
-		
+        Scanner teclado = new Scanner(System.in);
+        int dia = 0;
+        int mes = 0;
+        int anho = 0;
+        String fechaNacimiento = "";
+
+        //Pedimos la fecha de nacimiento por consola
+        System.out.println("**** CALCULA TU EDAD ******");
+        System.out.println("Introduce tu fecha de nacimiento:");
+        System.out.println("Día: ");
+        dia = teclado.nextInt();
+        System.out.println("Mes: ");
+        mes = teclado.nextInt();
+        System.out.println("Año: ");
+        anho = teclado.nextInt();
+
+		LocalDate fecha = LocalDate.of(anho, mes, dia);
+		//Lo pasamos a String para pasarlo como argumento (String[] args)
+		fechaNacimiento = fecha.toString();
+
+
         //Crea e inicia un proceso que estará formado de argumentos
-		process = new ProcessBuilder(args).start();
-		//Primero se ponen las excepciones específicas
-	} catch (ArrayIndexOutOfBoundsException e) {
-		System.out.println("Tienes que llamar al proceso dado pq no has pasado argumentos");
+        //El proceso se inicia fuera del try-with-resources
+        Process proceso = new ProcessBuilder(args).start();
+        	//cañería
+        try (InputStream is = proceso.getInputStream(); 
+			//char
+            InputStreamReader isr = new InputStreamReader(is); 
+			//líneas
+            BufferedReader br = new BufferedReader(isr); 
+	){
+              
+	System.out.println("Salida"+ Arrays.toString(args));
+	String line = "";
 
-		System.exit(0);//cierra el hilo ppal y cierra el programa
-	//Luego las excepciones genéricas pq sino Excepcion captura todo y 
-	//no lo diferencia
-
-	//Se pueden llamar e pq son ámbitos diferentes
-	} catch (IOException e1){
-		System.out.println(e1.getLocalizedMessage());
-
-		System.exit(0);
-	}catch (Exception e){
-	e.printStackTrace();
+	while((line = br.readLine()) != null){
+		System.out.println(line);
 	}
-			
-    
-}
-}
+	
+		//Primero se ponen las excepciones específicas
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+        } catch (IOException e1) {
+            System.out.println(e1.getLocalizedMessage());
+
+            System.exit(0);
+        } catch (Exception e) {
+            e.printStackTrace();		
+        }
+
+    }
