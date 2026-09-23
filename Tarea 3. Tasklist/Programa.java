7c24a4927b63ed52dbf6981e33cb7fa04433221a
Andrea Barral Bello <andrea.barral.bello@gmail.com>
2026-09-23 19:33:12 +0200

    tasklit

--- Tarea 3. Tasklist/Programa.java
+++ Tarea 3. Tasklist/Programa.java
@@ -9,7 +9,7 @@ public class Programa {
 
     public static void main(String[] args) {
 
-        String [] lineaComando = {"java", "ClaseTasklist", "tasklist" };
+        String [] lineaComando = {"tasklist"};
 
         try(
         Process proceso = new ProcessBuilder(lineaComando).start();
@@ -22,8 +22,28 @@ public class Programa {
 
         System.out.println(Arrays.toString(lineaComando));
 
+        linea = br.readLine();
+        linea = br.readLine();
+        linea = br.readLine();
+
+        String [] cachos = linea.split(" ");
+
+        int primero = cachos [0].length();
+        int segundo = cachos [1].length();
+        int tercero = cachos [2].length();
+        int cuarto = cachos [3].length();
+
+
+
         while ((linea = br.readLine()) != null){
-            System.out.println(linea);
+            System.out.println("Nombre de imagen: "+linea.substring(0, cachos[0].length()));
+            //la segunda cadena empieza en la última posición de 0 +1
+            System.out.println("PID: "+linea.substring(((cachos [0].length()) -1),((cachos[0].length()+1)+(cachos[1].length()-1))));
+
+            System.out.println("Nombre de sesión: "+linea.substring(((cachos[0].length())+(cachos[1].length())+1)
+            , ((cachos[0].length())+(cachos[1].length())+ (cachos[2].length()))));
+
+            System.out.println("Nombre "+ (primero) + "Servicios" +linea.substring((primero), (primero+segundo+tercero+3)));
         }
         } catch (IOException ex){
             System.out.println(ex.getLocalizedMessage());
